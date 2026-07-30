bc4ad5882a535a65b5f7ce10df3f12e22c535241
krishnabhatt-5160 <bhattkrishna430@gmail.com>
2026-07-30 23:23:14 +0530

    Time: 0 ms (100%), Space: 9.1 MB (43.23%) - LeetHub

---
+++ 3993-maximum-value-of-an-alternating-sequence/3993-maximum-value-of-an-alternating-sequence.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    long long maximumValue(int n, int s, int m) {
+        if (n == 1) return s;
+
+        long long peaks = n / 2;
+        return 1LL * s + m + (peaks - 1) * 1LL * (m - 1);
+    }
+};
