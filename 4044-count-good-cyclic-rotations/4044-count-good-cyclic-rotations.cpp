3936fb38346c4f48f52e7d3075b0699e23c84f87
krishnabhatt-5160 <bhattkrishna430@gmail.com>
2026-09-06 23:01:16 +0530

    Time: 5 ms (39.27%), Space: 103.3 MB (38.74%) - LeetHub

--- 4044-count-good-cyclic-rotations/4044-count-good-cyclic-rotations.cpp
+++ 4044-count-good-cyclic-rotations/4044-count-good-cyclic-rotations.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
     int countGoodRotations(vector<int>& nums) {
-        int n = nums.size();
-
-        vector<long long> prefix(n + 1, 0);
-
-        for(int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + nums[i];
+        
+        long long n=nums.size();
+        if(n==0)return 0;
+        
+        long long leftsum=0,rightsum=0;
+        for(int i=0;i<n/2;i++)
+        {
+            leftsum+=nums[i];
+            rightsum+=nums[n-1-i];
         }
-
-        long long total = prefix[n];
-        int count = 0;
-
-        for(int i = 0; i < n; i++) {
-
-            // Take n/2 elements starting from i
-            int j = i + n / 2;
-
-            long long first;
-
-            if(j <= n) {
-                first = prefix[j] - prefix[i];
-            }
-            else {
-                first = (prefix[n] - prefix[i]) + prefix[j - n];
-            }
-
-            long long second = total - first;
-
-            if(first > second) {
-                count++;
-            }
+        int count=0;
+        int half=n/2;
+        for(int i=0;i<n;i++)
+        {
+            if(leftsum>rightsum)count++;
+            int left = nums[i];
+            int right = nums[(i + half) % n];
+            leftsum = leftsum - left + right;
+            rightsum = rightsum - right + left;
         }
-
         return count;
     }
 };
