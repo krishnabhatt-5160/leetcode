9d093b2f7520a89749357b6a7c9034a5b33af081
krishnabhatt-5160 <bhattkrishna430@gmail.com>
2026-09-09 03:01:12 +0530

    Time: 16 ms (80.25%), Space: 50.6 MB (29.24%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,82 @@
+class Trie {
+public:
+
+    struct Node {
+        Node* links[26];
+        bool flag = false;
+
+        bool constraintkey(char ch) {
+            return (links[ch - 'a'] != NULL);
+        }
+
+        void put(char ch, Node* node) {
+            links[ch - 'a'] = node;
+        }
+
+        Node* get(char ch) {
+            return links[ch - 'a'];
+        }
+
+        void setend() {
+            flag = true;
+        }
+
+        bool isend() {
+            return flag;
+        }
+    };
+
+    Node* root;
+
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+
+        Node* node = root;
+
+        for(int i = 0; i < word.length(); i++) {
+
+            if(!node->constraintkey(word[i])) {
+                node->put(word[i], new Node());
+            }
+
+            node = node->get(word[i]);
+        }
+
+        node->setend();
+    }
+    
+    bool search(string word) {
+
+        Node* node = root;
+
+        for(int i = 0; i < word.length(); i++) {
+
+            if(!node->constraintkey(word[i])) {
+                return false;
+            }
+
+            node = node->get(word[i]);
+        }
+
+        return node->isend();
+    }
+    
+    bool startsWith(string prefix) {
+
+        Node* node = root;
+
+        for(int i = 0; i < prefix.length(); i++) {
+
+            if(!node->constraintkey(prefix[i])) {
+                return false;
+            }
+
+            node = node->get(prefix[i]);
+        }
+
+        return true;
+    }
+};
